14e4ac8cca5b7a038dca27c4436f9e4de67c7bf8
Jack Meng <jackmeng0814@gmail.com>
2021-10-26 13:45:43 -0400

    Create Fetch-Saves.sh

---
+++ Fetch-Saves.sh
@@ -0,0 +1,55 @@
+# This Script for *Nix systems will help you automatically fetch this repository
+#
+# It will also constantly update the "cloned" repository everytime you run this script
+# - This means you must not have deleted the original folder that this script cloned to
+#
+# Notice: If you are trying to modify certain aspects of this script without knowledge 
+#         of what is happening within this script, please take extra caution or just don't
+#         as some properties can cause unwanted damage. 
+#
+# License: MIT License 
+#
+# Reuse permitted
+#
+# The below information is auto generated however modification to this part can make it
+# harder to debug issues. It is also for me easily compare this shell code with the automated
+# program that I used to generate these scripts
+#
+# Script_Informations:
+#   version: 1.0
+#   compatible-packages: [ git, git-lfs, ssh, vim, github-cli ]
+#   author: git@github.com:meng-jack
+#   shell-version: 1.3
+#   pull-trace: [ git@github.com:meng-jack/reactor-saves.git ]
+#   push-trace:
+#   github-cli: false
+#   git-dep-version: [ 4.2, 5.2.1 ]
+#   deploy-state: 
+#   app-version:
+#   bash-version: 5.1
+#   spprt: ${SYSTEM_VAR.ENV}
+#
+# Run this script using either of these commands:
+#   bash ./Fetch-Saves.sh
+#
+# OR
+#
+#   chmod +x Fetch-Saves.sh
+#   ./Fetch-Saves.sh
+#
+# Debugging Common Issues
+#   Notice: This is auto-generated and solutions discussed might not be compatible or of context
+#           with this script. 
+#   1) Unknown Package or Unknown Command | Solution: Install this package by using the proper *Nix package manager for example, debian:
+#           sudo apt-get install package_name
+#   2) Permission denied when running script | Solution: Use the option to run this script:
+#           bash ./file_name.sh
+#           OR 
+#           chmod +x ./file_name.sh
+# == END ==
+#
+# SCRIPT-GENERATION ERROR=>
+# Parameters for script generation met an error with attemtping to checkout working-tree
+# (1) ERRN CODE 504
+
+
